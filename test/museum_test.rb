0e293acee34131d51f8e7de4b446b5b6a6636f48
brickstar <brickstar@gmail.com>
2018-03-28 09:18:49 -0600

    museum name

---
+++ test/museum_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+require './lib/museum'
+
+class MuseumTest < Minitest::Test
+  def test_it_exists
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_instance_of Museum, dmns
+  end
+
+  def test_it_has_a_name
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    assert_equal "Denver Museum of Nature and Science", dmns.name
+  end
+end
