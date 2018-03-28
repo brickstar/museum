367f5d6079ea229006e1632c3ed2f504aaae234d
brickstar <brickstar@gmail.com>
2018-03-28 09:25:27 -0600

    add_exhibits

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -13,4 +13,22 @@ class MuseumTest < Minitest::Test
 
     assert_equal "Denver Museum of Nature and Science", dmns.name
   end
+
+  def test_it_starts_with_no_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    expected = {}
+    assert_equal expected, dmns.exhibits
+  end
+
+  def test_it_can_add_exhibits
+    dmns = Museum.new("Denver Museum of Nature and Science")
+
+    dmns.add_exhibit("Dead Sea Scrolls", 10)
+    dmns.add_exhibit("Gems and Minerals", 0)
+    
+    expected = {"Dead Sea Scrolls" => 10, "Gems and Minerals" => 0}
+
+    assert_equal expected, dmns.exhibits
+  end
 end
